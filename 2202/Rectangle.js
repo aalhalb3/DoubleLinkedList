95f125eb3d78630c8846be794e1308871254ee1f
aalhalb3 <71105430+aalhalb3@users.noreply.github.com>
2020-09-29 18:56:10 -0400

    Added the triangle function
    Added the triangle

--- 2202/Rectangle.js
+++ 2202/Rectangle.js
@@ -26,7 +26,24 @@ function rectangle(height , width, displacement){
         console.log(rectangle);
 }
 
+let triangle = function(height,displacement){
+    triangle =""
+    for (let h=0;h<height;h++){
+        let line = "\n";
 
+        let offset="";
+        for(let d=0;d<displacement;d++)
+            offset += " ";
+        line +=offset;
+
+        for (let s=0;s<h+1;s++)
+            line += "*";
+
+        triangle +=line;
+    }
+};
+triangle(10,10)
+console.log(triangle);
 
 shape(rectangle, 5, 5, 10);
 //shape(6, 6, 1, 1, 10);
